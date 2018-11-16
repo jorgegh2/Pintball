2dfc853628bc4ccd30f712109e41352a4208ea63
Jorge Gemas Herencia <jorgegh2@TR12.CAT>
2018-11-16 11:55:59 +0100

    todo 3 physics 3D

---
+++ Physics3D_class3_handout/PhysBody3D.cpp
@@ -0,0 +1,30 @@
+#include "PhysBody3D.h"
+#include "glmath.h"
+#include "Bullet/include/btBulletDynamicsCommon.h"
+
+// TODO 4: Fill in PhysBody methods. Remember to use 
+// get/setOpenGLMatrix methods
+
+// ---------------------------------------------------------
+PhysBody3D::PhysBody3D(btRigidBody* body)
+{}
+
+// ---------------------------------------------------------
+PhysBody3D::~PhysBody3D()
+{}
+
+// ---------------------------------------------------------
+btTransform PhysBody3D::GetTransform(float* matrix) const
+{
+	return body->getWorldTransform();
+}
+
+// ---------------------------------------------------------
+void PhysBody3D::SetTransform(const float* matrix) const
+{
+}
+
+// ---------------------------------------------------------
+void PhysBody3D::SetPos(float x, float y, float z)
+{
+}
